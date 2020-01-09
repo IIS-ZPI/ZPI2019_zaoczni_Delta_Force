9d9ed795d80b001e105a869f6516c396190d25f7
lukaszk960214 <212314@edu.p.lodz.pl>
2020-01-09 21:27:39 +0100

    difference

--- app/src/main/java/com/example/financialappnbp/Math.java
+++ app/src/main/java/com/example/financialappnbp/Math.java
@@ -1,4 +1,11 @@
 package com.example.financialappnbp;
+import com.example.financialappnbp.mathInterface.IArithmeticsDiff;
 
-public class Math {
+public class Math implements IArithmeticsDiff {
+
+
+    @Override
+    public Double difference(Double A, Double B) {
+        return A/B;
+    }
 }
---
+++ app/src/main/java/com/example/financialappnbp/mathInterface/IArithmeticsDiff.java
@@ -0,0 +1,5 @@
+package com.example.financialappnbp.mathInterface;
+
+public interface IArithmeticsDiff {
+    Double difference(Double A, Double B);
+}
