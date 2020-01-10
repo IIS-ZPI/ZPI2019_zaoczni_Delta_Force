8242fa3c9f3666b247d7888f78b6dfdf69158bc8
lukaszk960214 <212314@edu.p.lodz.pl>
2020-01-10 20:10:30 +0100

    add comment

--- app/src/main/java/com/example/financialappnbp/MainActivity.java
+++ app/src/main/java/com/example/financialappnbp/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Math math = new Math();
+        Math math = new Math();//komentarz
         System.out.println("dodawanie"+math.Addition(5,2));
         System.out.println("odejmowanie"+math.Difference(6,2));
         System.out.println("dzielenie"+math.Division(2,3));
