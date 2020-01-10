ff7e86a98f03be0d147e8cb88bb1697534ec4f8d
lukaszk960214 <212314@edu.p.lodz.pl>
2020-01-10 20:15:33 +0100

    add comment3

--- app/src/main/java/com/example/financialappnbp/MainActivity.java
+++ app/src/main/java/com/example/financialappnbp/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         System.out.println("dodawanie"+math.Addition(5,2));
         System.out.println("odejmowanie"+math.Difference(6,2));
         System.out.println("dzielenie"+math.Division(2,3));
-        System.out.println("mnozenie"+math.Multiplication(8,3));
+        System.out.println("mnozenie"+math.Multiplication(8,3));//komentarz3
 
 
     }
