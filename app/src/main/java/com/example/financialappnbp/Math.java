b786c8cdc85c2fd638a2558e50933df322247cfd
adann96 <domanski.adam_wojciech@poczta.onet.pl>
2020-01-10 20:28:21 +0100

    Added first commit to new branch randomly by cherry pick

---
+++ app/src/main/java/com/example/financialappnbp/Math.java
@@ -0,0 +1,31 @@
+package com.example.financialappnbp;
+import com.example.financialappnbp.mathInterface.IArithmeticsDiff;
+import com.example.financialappnbp.mathInterface.IArithmeticsDiv;
+import com.example.financialappnbp.mathInterface.IArithmeticsAdd;
+import com.example.financialappnbp.mathInterface.IArithmeticsMult;
+
+public class Math implements IArithmeticsDiv, IArithmeticsAdd, IArithmeticsMult, IArithmeticsDiff {
+
+    @Override
+    public double Division(double A, double B) {
+        return A / B; //Comment needed to accomplish the task
+    }
+
+    /*Try to multiply two values*/
+    @Override
+    public double Multiplication(double A, double B) {
+        return A * B;
+    }
+
+    /*Addition of two values*/
+    @Override
+    public double Addition(double A, double B) {
+        return A + B;
+    }
+
+    /*Difference between values a and b*/
+    @Override
+    public double Difference(double A, double B) {
+        return A-B;
+    }
+}
